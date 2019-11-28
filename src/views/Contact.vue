91cef2eca4da6145290bbd20e6d3b3000e6b0fd4
David Moftakhar <david@sellmysnaps.com>
2019-11-28 08:51:21 +0000

    Contact page setup and linked in navbar.

---
+++ src/views/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Contact us</h1>
+        <p>Complete the form below to contact us.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
